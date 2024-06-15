1f37c9b81faff2906ba2b2371b0a47251743cfbd
Enova <elijahbansley@gmail.com>
2024-06-14 19:29:31 -0500

    Bump version and changelog

--- PlayableEmbeds/build.gradle.kts
+++ PlayableEmbeds/build.gradle.kts
@@ -1,7 +1,9 @@
-version = "1.0.2"
+version = "1.0.3"
 description = "Makes Spotify and Youtube Embeds playable"
 
 aliucord.changelog.set("""
+    # 1.0.3
+    * All embedded videos are now playable in-line.
     # 1.0.2
     * Youtube embed now supports timestamps
 """.trimIndent())
